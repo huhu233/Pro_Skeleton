97c049f1ff408de2b13c885324caf0f93f3602b1
虎虎 张 <zhangtiehu_hdzx@163.com>
2017-06-02 15:00:01 +0800

    修正，添加了console提示

--- Pro_Skeleton/scheme1.cpp
+++ Pro_Skeleton/scheme1.cpp
@@ -1,17 +1,22 @@
 /*
+	平台:console程序
 	功能:实现关节点绘制，拖动
+	根据提示输入
 	trackbar:0，可绘制状态（无法拖拽），左键点击图片会生成相应的点
 	trackbar:1, 可拖动状态（无法绘制），右击关节点移动鼠标可进行拖拽，左击固定
-
+	根据提示保存，文件名按照系统时间命名
 */
+
+#include<Windows.h>
 #include<opencv2/core/core.hpp>
 #include<opencv2/highgui.hpp>
 #include<opencv/cv.hpp>
 #include<vector>
-#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<string>
 #include<cmath>
 #include"type.h"
-
 
 IplImage *img = NULL;
 IplImage *imgNew = NULL;
@@ -25,7 +30,7 @@ std::vector<int> tmp;
 bool visit[MAXNODE] = { false };		//访问标记
 bool lock_status = false;				//锁定标志：false-可绘制，true-可拖拽
 bool onMove = false;					//拖拽开关
-
+std::string outputPath = "D:\\project\\outPut\\";
 
 //鼠标相应事件
 void onMouse(int event, int x, int y, int flags, void *param);
@@ -42,25 +47,65 @@ void DFSRedraw(int u);
 
 int main()
 {
-	img = cvLoadImage("person.jpg", 1);
-	imgNew = (IplImage*)cvClone(img);
-	cvNamedWindow(WINNAME, 0);
-	cvCreateTrackbar(TRACKBARNAME, WINNAME, &trackValue, 1, onChange);
-	cvShowImage(WINNAME, imgNew);
-	cvSetMouseCallback(WINNAME, onMouse, 0);
-	cvWaitKey(0);
+	int choice, save;
+	char str[100];
+	printf("请输入选择0-结束，1-编辑: ");
+	scanf("%d", &choice);
+	while (choice)
+	{
+		system("cls");
+		printf("请输入照片路径: ");
+		scanf("%s", str);
+		img = cvLoadImage(str, 1);
+		if (img != NULL)						//确保图像路径正确
+		{
+			imgNew = (IplImage*)cvClone(img);
+			cvNamedWindow(WINNAME, 0);
+			cvCreateTrackbar(TRACKBARNAME, WINNAME, &trackValue, 1, onChange);
+			cvShowImage(WINNAME, imgNew);
+			cvSetMouseCallback(WINNAME, onMouse, 0);
+			cvWaitKey(0);
 
-	if (imgNew != NULL)
-	{
-		cvReleaseImage(&imgNew);
-		imgNew = NULL;
+			//保存图片选择
+			printf("是否保存？0-否，1-是: ");
+			scanf("%d", &save);
+			if (save)
+			{
+				SYSTEMTIME st;
+				char imgName[100];
+
+				GetLocalTime(&st);
+				sprintf(imgName, "%02d%02d%02d%02d%02d%02d", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+				cvSaveImage((outputPath + imgName + ".jpg").c_str(), imgNew);
+				printf("已保存\n");
+			}
+			else
+			{
+				printf("\a未保存!\n");
+			}
+
+			if (imgNew != NULL)
+			{
+				cvReleaseImage(&imgNew);
+				imgNew = NULL;
+			}
+			if (img != NULL)
+			{
+				cvReleaseImage(&img);
+				img = NULL;
+			}
+			cvDestroyWindow(WINNAME);
+		}
+		else
+		{
+			printf("\a路径错误\n");
+		}
+
+		printf("请继续输入选择:0-退出，1-继续编辑: ");
+		scanf("%d", &choice);
+		if (!choice) printf("正在退出...");
 	}
-	if (img != NULL)
-	{
-		cvReleaseImage(&img);
-		img = NULL;
-	}
-	cvDestroyWindow(WINNAME);
+	return 0;
 }
 
 //重置访问标记
@@ -109,7 +154,7 @@ void DFSRedraw(int u)
 		int v = Graph[u][i];
 		if (v != pre)
 		{
-			pre = u;			//
+			pre = u;			
 			DFSRedraw(v);
 		}
 	}
